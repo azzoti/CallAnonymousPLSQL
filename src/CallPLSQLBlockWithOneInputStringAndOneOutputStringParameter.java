c81267715e66f595cd19e2d8d34995e84642d6a6
Tim Azzopardi <tim@tigerfive.com>
2012-05-19 18:59:01 +0100

    tabs to spaces

--- src/CallPLSQLBlockWithOneInputStringAndOneOutputStringParameter.java
+++ src/CallPLSQLBlockWithOneInputStringAndOneOutputStringParameter.java
@@ -10,24 +10,24 @@ public class CallPLSQLBlockWithOneInputStringAndOneOutputStringParameter {
     // exception handlers MUST clean up connections statements and result sets.
     public static void main(String[] args) throws SQLException {
 
-    	DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+        DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
 
         final Connection c = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "manager");
-		String plsql = "" +
-		" declare " +  
-		"      p_id varchar2(20) := null; " +
-		" begin " +
-		"    p_id := ?; " +
-		"    ? := 'input parameter was = ' || p_id;" +
-		" end;";
+        String plsql = "" +
+        " declare " +  
+        "      p_id varchar2(20) := null; " +
+        " begin " +
+        "    p_id := ?; " +
+        "    ? := 'input parameter was = ' || p_id;" +
+        " end;";
         CallableStatement cs = c.prepareCall(plsql);
-		cs.setString(1, "12345");
-		cs.registerOutParameter(2, Types.VARCHAR);
-		cs.execute();
-		
-		System.out.println("Output parameter was = '" + cs.getObject(2) + "'");
-		
-		cs.close();
+        cs.setString(1, "12345");
+        cs.registerOutParameter(2, Types.VARCHAR);
+        cs.execute();
+        
+        System.out.println("Output parameter was = '" + cs.getObject(2) + "'");
+        
+        cs.close();
         c.close();
     }
 }
--- src/CallPLSQLBlockWithOneInputStringArrayAndOneOutputStringParameterAndOneOutputCursorParameter.java
+++ src/CallPLSQLBlockWithOneInputStringArrayAndOneOutputStringParameterAndOneOutputCursorParameter.java
@@ -13,52 +13,48 @@ public class CallPLSQLBlockWithOneInputStringArrayAndOneOutputStringParameterAnd
 
     public static void main(String[] args) throws Exception {
 
-    	DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
-    	
+        DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+        
         // Warning: this is a simple example program : In a long running application,
         // error handlers MUST clean up connections statements and result sets.
-    	
+        
         final Connection c = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "manager");
-		String plsql = "" +
-		" declare " +  
-		"    p_id string_array := null; " +
-		"	 l_rc sys_refcursor;" +
-		" begin " +
-		"    p_id := ?; " +
-		"    ? := 'input parameter first element was = ' || p_id(1);" +
-		"    open l_rc for select * from table(p_id) ; " +
-		"    ? := l_rc;" +
-		" end;";
+        String plsql = "" +
+        " declare " +  
+        "    p_id string_array := null; " +
+        "     l_rc sys_refcursor;" +
+        " begin " +
+        "    p_id := ?; " +
+        "    ? := 'input parameter first element was = ' || p_id(1);" +
+        "    open l_rc for select * from table(p_id) ; " +
+        "    ? := l_rc;" +
+        " end;";
 
-		String[] stringArray = new String[]{ "mathew", "mark"};
-		
-		// MUST CREATE THIS IN ORACLE BEFORE RUNNING
-		System.out.println("(This should be done once in Oracle)");
-		c.createStatement().execute("create or replace type string_array is table of varchar2(32)");
-		
-		ArrayDescriptor descriptor = ArrayDescriptor.createDescriptor( "STRING_ARRAY", c );
-		
-		Array array_to_pass = new ARRAY( descriptor, c, stringArray );
-		
-		CallableStatement cs = c.prepareCall(plsql);
-		cs.setArray( 1, array_to_pass );
-		cs.registerOutParameter(2, Types.VARCHAR);
-		cs.registerOutParameter(3, OracleTypes.CURSOR);
-		
-		cs.execute();
-		
-		System.out.println("Result = " + cs.getObject(2));
-		
-		ResultSet cursorResultSet = (ResultSet) cs.getObject(3);
-		while (cursorResultSet.next ())
-		{
-			System.out.println (cursorResultSet.getString(1));
-		} 
-		cs.close();
+        String[] stringArray = new String[]{ "mathew", "mark"};
+        
+        // MUST CREATE THIS IN ORACLE BEFORE RUNNING
+        System.out.println("(This should be done once in Oracle)");
+        c.createStatement().execute("create or replace type string_array is table of varchar2(32)");
+        
+        ArrayDescriptor descriptor = ArrayDescriptor.createDescriptor( "STRING_ARRAY", c );
+        
+        Array array_to_pass = new ARRAY( descriptor, c, stringArray );
+        
+        CallableStatement cs = c.prepareCall(plsql);
+        cs.setArray( 1, array_to_pass );
+        cs.registerOutParameter(2, Types.VARCHAR);
+        cs.registerOutParameter(3, OracleTypes.CURSOR);
+        
+        cs.execute();
+        
+        System.out.println("Result = " + cs.getObject(2));
+        
+        ResultSet cursorResultSet = (ResultSet) cs.getObject(3);
+        while (cursorResultSet.next ())
+        {
+            System.out.println (cursorResultSet.getString(1));
+        } 
+        cs.close();
         c.close();
     }
-
-
-	
-
 }
